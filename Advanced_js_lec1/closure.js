9231dd51ba76d7818228428ce4d0384b3d3aa6cd
iamharshgoel <harshgoel0012@gmail.com>
2021-03-03 21:22:09 +0530

    closure

---
+++ Advanced_js_lec1/closure.js
@@ -0,0 +1,15 @@
+function makeHelloFunction() {
+	var message = "Hello, world!";
+
+	function logMessage() {
+		console.log(message);
+	}
+	
+	return logMessage;
+}
+
+var sayHello = makeHelloFunction();
+
+sayHello();
+
+
